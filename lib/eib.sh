endlessm/eos-image-builder

commit c49160889b5d7b3a5babae656e4065a83fb5eecb

eib.sh: factor out retry logic

lib/eib.sh
@@ -174,28 +174,33 @@ eib_fail_publishing() {
   fi
 }
 
+# Work around transient failures
+eib_retry() {
+  local subcommand=${1?:No subcommand supplied to ${FUNCNAME}}
+  local i=0
+  local max_retries=10
+
+  while ! "$@" && (( ++i < max_retries )) ; do
+    echo "$@ failed; retrying..."
+    sleep 1
+  done
+
+  if (( i >= max_retries )); then
+    echo "$@ failed ${max_retries} times; giving up"
+    exit 1
+  fi
+}
+
 # Try to work around a race where partx sometimes reports EBUSY failure
 eib_partx_scan() {
   udevadm settle
-  local i=0
-  while ! partx -a -v "$1"; do
-	(( ++i ))
-	[ $i -ge 10 ] && break
-    echo "partx scan $1 failed, retrying..."
-    sleep 1
-  done
+  eib_retry partx -a -v "$1"
 }
 
 # Work around a race where loop deletion sometimes fails with EBUSY
 eib_delete_loop() {
   udevadm settle
-  local i=0
-  while ! losetup -d "$1"; do
-	(( ++i ))
-	[ $i -ge 10 ] && break
-    echo "losetup remove $1 failed, retrying..."
-    sleep 1
-  done
+  eib_retry losetup -d "$1"
 }
 
 recreate_dir() {

commit 907da006720af30c702240bade6da480857dbc21

eib.sh: fix ${parameter:?word} typo
Introduced in c491608, as part of
https://github.com/endlessm/eos-image-builder/pull/197

The typoed form, ${parameter?:word}, treats the colon as part of the
error message, and only tests whether 'parameter' is unset, not whether
it is null:

    $ echo ${x?:wrong}
    bash: x: :wrong
    $ echo ${x:?right}
    bash: x: right
    $ x=
    $ echo ${x?:wrong}

    $ echo $?
    0

It's probably an error for the first term of eib_retry's subcommand to
be the empty string, so :? seems more appropriate.

lib/eib.sh
@@ -176,7 +176,7 @@ eib_fail_publishing() {
 
 # Work around transient failures
 eib_retry() {
-  local subcommand=${1?:No subcommand supplied to ${FUNCNAME}}
+  local subcommand=${1:?No subcommand supplied to ${FUNCNAME}}
   local i=0
   local max_retries=10
 