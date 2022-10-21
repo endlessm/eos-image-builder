53fd0259a1b171519a90a2e61c387a1681ba4ea1
Dan Nicholson <dbn@endlessos.org>
2022-10-21 07:41:44 -0600

    Cleanup some comments and documentation
    These were found in a post-merged review by @wjt. Thanks!
    
    https://phabricator.endlessm.com/T33827
    (cherry picked from commit c2b150a41b6c2d0b3672955161f2ce7ef91bd237)

--- lib/eibflatpak.py
+++ lib/eibflatpak.py
@@ -691,7 +691,7 @@ class FlatpakManager(object):
             )
 
     def _check_excluded_operations(self, operations):
-        """Verify none of the refs in the opertions are excluded"""
+        """Verify none of the refs in the operations are excluded"""
         excluded = []
         extra_data = []
         eol = []
